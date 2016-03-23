5e26821ce209ee904358e7c92e90e63dd7846f13
Barbara Krautgartner <barbrara.krautgartner@oeaw.ac.at>
2016-03-23 16:52:17 +0100

    added setting for local environment

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 $runner = true;
 
 require_once __DIR__ . '/corpus_shell/vendor/autoload.php';
+//require_once __DIR__ . '/corpus_shell/modules/fcs-aggregator/switch.php';
 chdir(__DIR__ . '/corpus_shell/modules/fcs-aggregator');
 require_once 'switch.php';
 
@@ -16,11 +17,32 @@ $sru_fcs_params->version = '1.2';
 $sru_fcs_params->xdataview = 'kwic,title';
 $sru_fcs_params->maximumRecords = '10';
 switch ($_SERVER['SERVER_NAME']) {
-
-    case 'localhost':
+	
+    case 'persengldict.hephaistos.arz.oeaw.ac.at':
+// Set up the parameter object
+// x-format=htmlpagetable&operation=searchRetrieve&version=1.2&
+// x-dataview=kwic,title&maximumRecords=10&x-context=pes_eng_032&
+// query=senses==Persian
+$sru_fcs_params->xcontext = 'pes_eng_032';
+$sru_fcs_params->maximumRecords = '50';
+$sru_fcs_params->query = 'lemma==fārsi';
+break;
+    case 'zulu.hephaistos.arz.oeaw.ac.at':
+$sru_fcs_params->maximumRecords = '50';
+$sru_fcs_params->xcontext = 'zul_eng_016';
+$sru_fcs_params->query = 'lemma=zulu';
+break;
+    case 'damasz.hephaistos.arz.oeaw.ac.at':
+$sru_fcs_params->maximumRecords = '50';
 $sru_fcs_params->xcontext = 'apc_eng_002';
 $sru_fcs_params->query = 'sense=Damascene';
 break;
+
+   case 'localhost':
+$sru_fcs_params->xcontext = 'apc_eng_002';
+$sru_fcs_params->query = 'sense=Damascene';
+break;
+
 }
 $sru_fcs_params->context = array($sru_fcs_params->xcontext);
 $s = new FCSSwitch();
