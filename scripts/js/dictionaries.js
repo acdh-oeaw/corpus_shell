9b9b711c5b29e29a06289217b7c8c4de29090b09
Barbara Krautgartner <barbrara.krautgartner@oeaw.ac.at>
2016-03-24 15:51:51 +0100

    added auto-complete function added function asking user language

--- scripts/js/dictionaries.js
+++ scripts/js/dictionaries.js
@@ -1,30 +1,51 @@
-$('document').ready(function() {
-	$(".nav").on("click","li",function() {
-		var index = $(this).index() + 2;
-		$("#main > div").hide();
-		$("#main > div:nth-child("+index+")").show();
-		});
+	$('document').ready(function() {
+		var userLang = navigator.language || navigator.userLanguage; 
+		var userLangAbbr = userLang.substr(0,2);
 		
-		$(".jumbotron").click(function(){
-		$("#main > div").hide();
-		$("#front").show();	
-		});
+		VirtualKeyboard.attachKeyboards();
+		$( "#query-text-ui" ).autocomplete({
+			source: function (request, response) {
+			$.getJSON( window.location.href+"/corpus_shell//modules/fcs-aggregator/switch.php?version=1.2&operation=scan&scanClause=sense-"+userLangAbbr+"="+$("#query-text-ui").val()+"&x-context="+xcontext+"&x-format=json&x-dataview=kwic,title&sort=x&maximumTerms=10&responsePosition=0", function (data) {
+				
+				
+				response($.map(data.terms, function (item) {
+					return {
+						label: item.value,
+						value: item.key
+					};
+				}));
+			});
+		},
+		minLength: 2,
+		delay: 100
+			});
 		
-		$("body").on("submit","form",function(event){
-			event.preventDefault();
-			$("#submit-query").hide();
-			$("#loader").show();
-         	var a = $("#query-text-ui").val();
-         	var o = $("#operator").val();
-    		var f = $("#field").val();
-			field = f;
-         	$("#searchcontainer").load(window.location.href+"/corpus_shell/modules/fcs-aggregator/switch.php?version=1.2&operation=searchRetrieve&query="+f+o+encodeURIComponent(a)+"&x-context="+xcontext+"&startRecord=1&maximumRecords=10&x-format=htmlpagetable&x-dataview=kwic,title #result-container",function(){
-				 $("#query-text-ui").val(a);
-				 $("#operator").val(o);
-				 $("#field").val(f);
-				 $("#loader").hide();
-				 $("#submit-query").show();
-				 
-			 });
+		$(".nav").on("click","li",function() {
+			var index = $(this).index() + 2;
+			$("#main > div").hide();
+			$("#main > div:nth-child("+index+")").show();
+			});
+			
+			$(".jumbotron").click(function(){
+			$("#main > div").hide();
+			$("#front").show();	
+			});
+			  
+			$("body").on("submit","form",function(event){
+				event.preventDefault();
+				$("#submit-query").hide();
+				$("#loader").show();
+				var a = $("#query-text-ui").val();
+				var o = $("#operator").val();
+				var f = $("#field").val();
+				field = f;
+				$("#searchcontainer").load(window.location.href+"/corpus_shell/modules/fcs-aggregator/switch.php?version=1.2&operation=searchRetrieve&query="+f+o+encodeURIComponent(a)+"&x-context="+xcontext+"&startRecord=1&maximumRecords=10&x-format=htmlpagetable&x-dataview=kwic,title #result-container",function(){
+					 $("#query-text-ui").val(a);
+					 $("#operator").val(o);
+					 $("#field").val(f);
+					 $("#loader").hide();
+					 $("#submit-query").show();
+					 
+				 });
+			});
 		});
-	});
