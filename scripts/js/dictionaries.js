5937383ab9e2422308ca7126b5a3c92df2030341
Barbara Krautgartner <barbrara.krautgartner@oeaw.ac.at>
2016-03-31 17:00:07 +0200

    improved autocomplete to query for different indexes

--- scripts/js/dictionaries.js
+++ scripts/js/dictionaries.js
@@ -4,27 +4,43 @@
     });
 		var userLang = navigator.language || navigator.userLanguage; 
 		var userLangAbbr = userLang.substr(0,2);
+		var indexes;
+		
+		var selectedIndex;
 		
 		VirtualKeyboard.attachKeyboards();
+		 $.ajax({
+			url: window.location.href+"corpus_shell//modules/fcs-aggregator/switch.php?version=1.2&operation=explain&x-context=apc_eng_002&x-format=json",
+			}).done(function(data) {
+				
+				indexes = data.indexes;
+});
 		
 		$( "#query-text-ui" ).autocomplete({
 			source: function (request, response) {
-			$.getJSON( window.location.href+"/corpus_shell//modules/fcs-aggregator/switch.php?version=1.2&operation=scan&scanClause=sense-"+userLangAbbr+"="+$("#query-text-ui").val()+"&x-context="+xcontext+"&x-format=json&x-dataview=kwic,title&sort=x&maximumTerms=10&responsePosition=0", function (data) {
+				$.each(indexes,function(key,value){
+					if (value.name == "lemma" || value.name == "inflected" || value.name.substring(0,5) == "sense" ){
+			$.getJSON( window.location.href+"/corpus_shell//modules/fcs-aggregator/switch.php?version=1.2&operation=scan&scanClause="+value.name+"="+$("#query-text-ui").val()+"&x-context="+xcontext+"&x-format=json&x-dataview=kwic,title&sort=x&maximumTerms=10&responsePosition=0", function (data) {
 				
 				
 				response($.map(data.terms, function (item) {
 					return {
 						label: item.value,
-						value: item.key
+						value: item.key,
+						href: item.nextHref
 					};
 				}));
+				
+			});
+			}
 			});
 		},
 		minLength: 1,
-		delay: 100,
+		delay: 1200,
 		select: function(event, ui) {
 			if(ui.item){
 				$('#query-text-ui').val(ui.item.value);
+				href = ui.item.href;
 				}
 				$('#searchretrieve').submit();
 		}
@@ -46,33 +62,41 @@
 				$("#submit-query").hide();
 				$("#loader").show();
 				var a = $("#query-text-ui").val();
-				var o = $("#operator").val();
-				var f = $("#field").val();
-				field = f;
-				$("#searchcontainer").load(window.location.href+"/corpus_shell/modules/fcs-aggregator/switch.php?version=1.2&operation=searchRetrieve&query=sense-"+userLangAbbr+"="+encodeURIComponent(a)+"&x-context="+xcontext+"&startRecord=1&maximumRecords=50&x-format=htmlpagetable&x-dataview=kwic,title #result-container",function(){
+				var href = window.location.href+"/corpus_shell/modules/fcs-aggregator/switch.php?version=1.2&operation=searchRetrieve&query="+encodeURIComponent(a)+"&x-context="+xcontext+"&startRecord=1&maximumRecords=50&x-format=htmlpagetable&x-dataview=kwic,title";
+
+				if (selectedIndex != ''){
+					
+					console.log(href);
+				}
+				$("#searchcontainer").load(href + " #result-container",function(){
 					 $("#query-text-ui").val(a);
-					 $("#operator").val(o);
-					 $("#field").val(f);
 					 $("#loader").hide();
 					 $("#submit-query").show();
 					 $( "#query-text-ui" ).autocomplete({
-			source: function (request, response) {
-			$.getJSON( window.location.href+"/corpus_shell//modules/fcs-aggregator/switch.php?version=1.2&operation=scan&scanClause=sense-"+userLangAbbr+"="+$("#query-text-ui").val()+"&x-context="+xcontext+"&x-format=json&x-dataview=kwic,title&sort=x&maximumTerms=10&responsePosition=0", function (data) {
+				source: function (request, response) {
+				$.each(indexes,function(key,value){
+					if (value.name == "lemma" || value.name == "inflected" || value.name.substring(0,5) == "sense" ){
+			$.getJSON( window.location.href+"/corpus_shell//modules/fcs-aggregator/switch.php?version=1.2&operation=scan&scanClause="+value.name+"="+$("#query-text-ui").val()+"&x-context="+xcontext+"&x-format=json&x-dataview=kwic,title&sort=x&maximumTerms=10&responsePosition=0", function (data) {
 				
 				
 				response($.map(data.terms, function (item) {
 					return {
 						label: item.value,
-						value: item.key
+						value: item.key,
+						href: item.nextHref
 					};
 				}));
+				
+			});
+			}
 			});
 		},
 		minLength: 1,
-		delay: 100,
+		delay: 1200,
 		select: function(event, ui) {
 			if(ui.item){
 				$('#query-text-ui').val(ui.item.value);
+				href = ui.item.href;
 				}
 				$('#searchretrieve').submit();
 		}
