d92b631d4d46b69a25b95f936db50b9aaa917705
Omar Siam <Omar.Siam@oeaw.ac.at>
2017-03-13 15:44:43 +0100

    Update config with the one the server understands today.

--- scripts/js/corpusshell.config.js
+++ scripts/js/corpusshell.config.js
@@ -44,90 +44,53 @@
  */
 var SearchConfig = new Array();
 SearchConfig[0] = new Array();
-SearchConfig[0]['x-context'] = 'arz_eng_006';
-SearchConfig[0]['DisplayText'] = 'VICAV dictionary Cairo-dialect';
+SearchConfig[0]['x-context'] = 'vicav_profiles_001';
+SearchConfig[0]['DisplayText'] = 'Profiles';
 
 SearchConfig[1] = new Array();
-SearchConfig[1]['x-context'] = 'apc_eng_002';
-SearchConfig[1]['DisplayText'] = 'VICAV dictionary Damascus-dialect';
+SearchConfig[1]['x-context'] = 'vicav_sampletexts';
+SearchConfig[1]['DisplayText'] = 'Sample Texts';
 
 SearchConfig[2] = new Array();
-SearchConfig[2]['x-context'] = 'deu_wikt_002';
-SearchConfig[2]['DisplayText'] = 'Wiktionary';
+SearchConfig[2]['x-context'] = 'vicav_meta';
+SearchConfig[2]['DisplayText'] = 'Meta Texts';
 
 SearchConfig[3] = new Array();
-SearchConfig[3]['x-context'] = 'clarin.at:icltt:ddc';
-SearchConfig[3]['DisplayText'] = 'DDC on Corpus3';
+SearchConfig[3]['x-context'] = 'vicav_tools_001';
+SearchConfig[3]['DisplayText'] = 'Tools Texts';
 
 SearchConfig[4] = new Array();
-SearchConfig[4]['x-context'] = 'clarin.at:icltt:ddc:traum_deu';
-SearchConfig[4]['DisplayText'] = ' -> Freud: Die Traumdeutung, German';
+SearchConfig[4]['x-context'] = 'vicav_lingfeatures_001';
+SearchConfig[4]['DisplayText'] = 'Linguistic Feature Texts';
 
 SearchConfig[5] = new Array();
-SearchConfig[5]['x-context'] = 'clarin.at:icltt:ddc:traum_russ';
-SearchConfig[5]['DisplayText'] = ' -> Freud: Die Traumdeutung, Russian';
+SearchConfig[5]['x-context'] = 'vicav_bibl_002';
+SearchConfig[5]['DisplayText'] = 'Bibliography';
 
 SearchConfig[6] = new Array();
-SearchConfig[6]['x-context'] = 'clarin.at:icltt:ddc:fackel';
-SearchConfig[6]['DisplayText'] = ' -> Die Fackel';
+SearchConfig[6]['x-context'] = 'arz_eng_006';
+SearchConfig[6]['DisplayText'] = 'Dictionary Cairo Variety';
 
 SearchConfig[7] = new Array();
-SearchConfig[7]['x-context'] = 'clarin.at:icltt:ddc:barock';
-SearchConfig[7]['DisplayText'] = ' -> Barocktexte';
+SearchConfig[7]['x-context'] = 'aeb_eng_001';
+SearchConfig[7]['DisplayText'] = 'TUNICO Dictionary';
 
 SearchConfig[8] = new Array();
-SearchConfig[8]['x-context'] = 'clarin.at:icltt:ddc06:c4';
-SearchConfig[8]['DisplayText'] = ' -> C4';
+SearchConfig[8]['x-context'] = 'aeb_eng_001__v001';
+SearchConfig[8]['DisplayText'] = 'Dictionary Tunis Variety';
 
 SearchConfig[9] = new Array();
-SearchConfig[9]['x-context'] = 'clarin.at:icltt:ddc06:c4_vienna';
-SearchConfig[9]['DisplayText'] = ' -> C4 Vienna';
+SearchConfig[9]['x-context'] = 'apc_eng_002';
+SearchConfig[9]['DisplayText'] = 'Dictionary Damascus Variety';
 
 SearchConfig[10] = new Array();
-SearchConfig[10]['x-context'] = 'sru-gutenberg';
-SearchConfig[10]['DisplayText'] = 'Gutenberg Metadata';
+SearchConfig[10]['x-context'] = 'ar_de__v001';
+SearchConfig[10]['DisplayText'] = 'Dictionary Standard Arabic';
 
 SearchConfig[11] = new Array();
-SearchConfig[11]['x-context'] = 'vicav-profile';
-SearchConfig[11]['DisplayText'] = 'VICAV Profile';
+SearchConfig[11]['x-context'] = 'tunico_conc';
+SearchConfig[11]['DisplayText'] = 'Tunico Concordance';
 
 SearchConfig[12] = new Array();
-SearchConfig[12]['x-context'] = 'vicav-bib';
-SearchConfig[12]['DisplayText'] = 'VICAV Bibliography';
-
-SearchConfig[13] = new Array();
-SearchConfig[13]['x-context'] = 'vicav-text';
-SearchConfig[13]['DisplayText'] = 'VICAV Sampletexte';
-
-SearchConfig[14] = new Array();
-SearchConfig[14]['x-context'] = 'ids-goethe';
-SearchConfig[14]['DisplayText'] = 'IDS Goethe';
-
-SearchConfig[15] = new Array();
-SearchConfig[15]['x-context'] = 'arz_eng_006_newStyle';
-SearchConfig[15]['DisplayText'] = 'VICAV dictionary Cairo-dialect newStyle';
-
-SearchConfig[16] = new Array();
-SearchConfig[16]['x-context'] = 'clarin.at:icltt:cr';
-SearchConfig[16]['DisplayText'] = 'ICLTT Content Repository';
-
-SearchConfig[17] = new Array();
-SearchConfig[17]['x-context'] = 'clarin.at:icltt:cr:stb';
-SearchConfig[17]['DisplayText'] = ' -> Schnitzler Tagebuch';
-
-SearchConfig[18] = new Array();
-SearchConfig[18]['x-context'] = 'clarin.at:icltt:cr:barock';
-SearchConfig[18]['DisplayText'] = ' -> Barock Texte';
-
-SearchConfig[19] = new Array();
-SearchConfig[19]['x-context'] = 'clarin.at:icltt:cr:aac-names';
-SearchConfig[19]['DisplayText'] = ' -> Namensdatenbank';
-
-SearchConfig[20] = new Array();
-SearchConfig[20]['x-context'] = 'clarin-d:digilib';
-SearchConfig[20]['DisplayText'] = 'TextGrid Digital Library';
-
-SearchConfig[21] = new Array();
-SearchConfig[21]['x-context'] = 'clarin-d:tueba-ddc';
-SearchConfig[21]['DisplayText'] = 'Tuebingen';
-
+SearchConfig[12]['x-context'] = 'tunico';
+SearchConfig[12]['DisplayText'] = 'Tunico Texts';
